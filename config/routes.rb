2d4c07579ddf8d8c6943410305f6d60faaeafb29
Julius Bzozowski <jbzozowski@gmail.com>
2017-11-01 22:27:41 -0400

    Added background image, nav bar with search, link quote to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :categories
   resources :quotes
+
+
+  get 'welcome/index'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
