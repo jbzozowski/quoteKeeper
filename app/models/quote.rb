1ef20d7fa91860f725800e11bb4d2aaaa183cca5
Julius Bzozowski <jbzozowski@gmail.com>
2017-11-06 23:19:36 -0500

    Added Sort by dropdown and added previous/next option to show quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,12 @@ class Quote < ApplicationRecord
   def self.search(search)
   where("quote ILIKE ? OR author ILIKE ? OR category ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
+
+  def next
+    Quote.where("id > ?", id).order("id ASC").first || Quote.first
+  end
+
+  def previous
+    Quote.where("id < ?", id).order("id DESC").first || Quote.last
+  end
 end
