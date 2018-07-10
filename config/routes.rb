9d3c7a8892cb50f394584dc5dda8d6717df6ab61
Julius Bzozowski <jbzozowski@gmail.com>
2018-07-10 17:33:16 -0400

    Added public quotes endpoints(show, index)

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     namespace :api, defaults: { format: :json },constraints: { subdomain: 'api'}, path: '/' do
 
       scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+        resources :quotes, :only => [:show, :index]
 
       end
 
