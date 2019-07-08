e11c3d6ef55c4f999dfd1afbcc65a083975c7b3b
Julius Bzozowski <jbzozowski@gmail.com>
2019-07-08 12:29:29 -0400

    deleting admin functions from views

--- app/admin/quotes.rb
+++ app/admin/quotes.rb
@@ -1,7 +1,5 @@
 ActiveAdmin.register Quote do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
+
 permit_params :quote, :author
 
 end
